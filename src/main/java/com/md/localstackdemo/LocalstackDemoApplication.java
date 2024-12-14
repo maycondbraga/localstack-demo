bf8f89b889b5d1c4de32bd1e1b73896180594cb3
maycondbraga <maycondbraga.s@gmail.com>
2024-12-14 00:26:15 -0300

    feat: initial commit

---
+++ src/main/java/com/md/localstackdemo/LocalstackDemoApplication.java
@@ -0,0 +1,13 @@
+package com.md.localstackdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalstackDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalstackDemoApplication.class, args);
+    }
+
+}
